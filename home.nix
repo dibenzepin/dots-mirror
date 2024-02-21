0a3e539b7cfe69c120f24378a2a93eb87c147492
poopsicles <87488715+poopsicles@users.noreply.github.com>
2024-02-21 22:51:27 +0100

    added git config

--- home.nix
+++ home.nix
@@ -3,4 +3,15 @@
     home.homeDirectory = "/home/fumnanya";
     home.stateVersion = "23.11";
     programs.home-manager.enable = true;
+    targets.genericLinux.enable = true;
+
+    programs.git = {
+        enable = true;
+        userName = "poopsicles";
+        userEmail = "87488715+poopsicles@users.noreply.github.com";
+
+        extraConfig = {
+            init.defaultBranch = "main";
+        };
+    };
 }
