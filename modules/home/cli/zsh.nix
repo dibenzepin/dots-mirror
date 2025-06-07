df69c2a62b9f8be68e9098cd503d6d232de316f4
fumnanya <fmowete@outlook.com>
2025-06-07 22:22:06 +0100

    drop zplug

--- modules/home/cli/zsh.nix
+++ modules/home/cli/zsh.nix
@@ -31,20 +31,38 @@
         };
       };
 
-      # TODO: replace with a more nix-y way?
-      zplug = {
-        enable = true;
-        plugins = [
-          {
-            name = "aaronkollasch/zsh-autocomplete"; # workaround for https://github.com/marlonrichert/zsh-autocomplete/issues/741
-            tags = [ "at:d53d90dd205b3ef66101d4cf8692c8518d4daf61" ];
-          }
-          {
-            name = "niraami/zsh-auto-notify"; # workaround for https://github.com/MichaelAquilina/zsh-auto-notify/pull/49
-            tags = [ "at:f1b54479d2db1002f8823d1217509b3e29015acd" ];
-          }
-        ];
-      };
+      plugins = [
+        {
+          # workaround for https://github.com/marlonrichert/zsh-autocomplete/issues/741
+          name = "zsh-autocomplete";
+          src = pkgs.fetchFromGitHub {
+            owner = "aaronkollasch";
+            repo = "zsh-autocomplete";
+            rev = "d53d90dd205b3ef66101d4cf8692c8518d4daf61";
+            hash = "sha256-kUQ1N/XJqn0y8ZHWWeo3PNoD0G6NZyx30YW9Pf5rAv4=";
+          };
+        }
+        {
+          # workaround for https://github.com/MichaelAquilina/zsh-auto-notify/pull/49
+          name = "auto-notify";
+          src = pkgs.fetchFromGitHub {
+            owner = "niraami";
+            repo = "zsh-auto-notify";
+            rev = "f1b54479d2db1002f8823d1217509b3e29015acd";
+            hash = "sha256-17w+I74Cgo9n73gZvVRNO2sWEcbbEH/TnyaIJJxEG8M=";
+          };
+        }
+        {
+          # from https://github.com/direnv/direnv/issues/443#issuecomment-2380714786
+          name = "zsh-completion-sync";
+          src = pkgs.fetchFromGitHub {
+            owner = "BronzeDeer";
+            repo = "zsh-completion-sync";
+            rev = "f6e95baf8cd87d9065516d1fa0bf0cb33b4235f3";
+            hash = "sha256-XhZ7l8e2H1+W1oUkDrr8pQVPVbb3+1/wuu7MgXsTs+8=";
+          };
+        }
+      ];
 
       initContent = ''
         setopt nomatch notify interactivecomments
