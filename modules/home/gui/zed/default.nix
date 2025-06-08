d5f4affd964a95deaceff92f87487608ea3dca3c
fumnanya <fmowete@outlook.com>
2025-06-08 02:08:45 +0100

    fix zed thing, ish

--- modules/home/gui/zed/default.nix
+++ modules/home/gui/zed/default.nix
@@ -19,16 +19,21 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    programs.zed-editor = {
-      enable = true;
-      extraPackages = with pkgs; [
-        nil
-        nixd
-        nixfmt-rfc-style
-      ];
-    };
+    programs.zed-editor.enable = true;
 
     catppuccin.zed.enable = false; # we handle this already
     xdg.configFile."zed/settings.json".source = config.lib.file.mkOutOfStoreSymlink cfg.path;
+
+    # we'd use extraPackages, but then the GUI launcher wouldn't see them
+    home.activation.updateZedLSPs = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      if [ -n "${cfg.path}" ] && [ -f "${cfg.path}" ]; then
+        run ${pkgs.jq}/bin/jq \
+          '.lsp.nil.binary.path = "${pkgs.nil}/bin/nil" |
+           .lsp.nixd.binary.path = "${pkgs.nixd}/bin/nixd" |
+           .lsp.nil.initialization_options.formatting.command = ["${pkgs.nixfmt-rfc-style}/bin/nixfmt"]' \
+          "${cfg.path}" > "${cfg.path}.tmp" && \
+          run mv "${cfg.path}.tmp" "${cfg.path}"
+      fi
+    '';
   };
 }
