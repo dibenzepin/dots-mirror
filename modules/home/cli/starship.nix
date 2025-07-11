2fa341222a45f8644557cb5c2b138f5ad79ff046
fumnanya <fmowete@outlook.com>
2025-07-11 13:36:43 +0100

    remove starship transient prompt, it's not worth it
    https://github.com/ghostty-org/ghostty/discussions/7450

--- modules/home/cli/starship.nix
+++ modules/home/cli/starship.nix
@@ -29,50 +29,14 @@
     };
 
     programs.zsh.initContent = ''
-      ############################## extra starship zsh config ##############################    
+      ############################## extra starship zsh config ##############################
 
       # add a blank line between prompts because starship's add_newline is annoying on first open
       precmd() { precmd() { echo "" } }
       alias clear='precmd() { precmd() { echo "" } } && clear'
       alias reset='precmd() { precmd() { echo "" } } && reset'
 
-      # fake transient for starship
-      # https://github.com/romkatv/powerlevel10k/issues/888
-      # https://github.com/starship/starship/issues/888#issuecomment-2239111488
-      # https://vincent.bernat.ch/en/blog/2021-zsh-transient-prompt
-      zle-line-init() {
-        [[ $CONTEXT == start ]] || return 0
-
-        # we're in line editing mode
-        while true; do
-          zle .recursive-edit
-          local -i ret=$?
-
-          # exit on EOT
-          [[ $ret == 0 && $KEYS == $'\4' ]] || break
-          [[ -o ignore_eof ]] || exit 0
-        done
-
-        # save prompt and shorten current prompt
-        local saved_prompt=$PROMPT
-        # local saved_rprompt=$RPROMPT
-        PROMPT=$(starship module character --status="$STARSHIP_CMD_STATUS")
-        # RPROMPT='''
-        zle .reset-prompt
-        PROMPT=$saved_prompt
-        # RPROMPT=$saved_rprompt
-
-        if (( ret )); then
-          zle .send-break # ctrl-c
-        else
-          zle .accept-line # enter
-        fi
-        return ret
-      }
-
-      zle -N zle-line-init # call this on every line
-
-      ############################ end extra starship zsh config ############################    
+      ############################ end extra starship zsh config ############################
     '';
   };
 }
