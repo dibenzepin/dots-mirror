b73bc7fd025acb1434b6205e8d62503bf326e1bc
fumnanya <fmowete@outlook.com>
2025-07-23 00:07:10 +0100

    jujutsu

--- modules/home/cli/git.nix
+++ modules/home/cli/git.nix
@@ -1,19 +1,86 @@
 {
   lib,
-  pkgs,
   config,
   ...
 }:
 
 {
   options = {
-    my.git.enable = lib.mkEnableOption "home-manager managed git";
+    my.git.enable = lib.mkEnableOption "home-manager managed git and jujutsu";
   };
 
   config = lib.mkIf config.my.git.enable {
-    home.packages = [
-      pkgs.jujutsu
-    ];
+    programs.jujutsu = {
+      enable = true;
+      settings = {
+        user.name = "fumnanya";
+        user.email = "fmowete@outlook.com";
+        ui.paginate = "never";
+        # templates.git_push_bookmark = ''"fumnanya/push-" ++ change_id.short()'';
+        signing = {
+          backend = "ssh";
+          behavior = "own";
+          key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBGPBbDGOYi1I65IKGnD1R2SYF83FDRZSAblkvI0AKY2";
+        };
+      };
+    };
+
+    # from https://github.com/jj-vcs/jj/wiki/Starship
+    programs.starship.settings = {
+      git_branch.disabled = true;
+      git_commit.disabled = true;
+      git_state.disabled = true;
+      git_metrics.disabled = true;
+      git_status.disabled = true;
+
+      custom =
+        lib.listToAttrs (
+          map
+            (module: {
+              name = module;
+              value = {
+                when = true;
+                command = "jj root >/dev/null 2>&1 || starship module ${module}";
+                description = "Only show ${module} if we're not in a jj repo";
+              };
+            })
+            [
+              "git_branch"
+              "git_commit"
+              "git_state"
+              "git_metrics"
+              "git_status"
+            ]
+        )
+        // {
+          jj = {
+            ignore_timeout = true;
+            description = "The current jj status";
+            detect_folders = [ ".jj" ];
+            symbol = "🥋 ";
+            command = ''
+              jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+                separate(" ",
+                  change_id.shortest(4),
+                  bookmarks,
+                  "|",
+                  concat(
+                    if(conflict, "💥"),
+                    if(divergent, "🚧"),
+                    if(hidden, "👻"),
+                    if(immutable, "🔒"),
+                  ),
+                  raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
+                  raw_escape_sequence("\x1b[1;32m") ++ coalesce(
+                    truncate_end(29, description.first_line(), "…"),
+                    "(no description set)",
+                  ) ++ raw_escape_sequence("\x1b[0m"),
+                )
+              '
+            '';
+          };
+        };
+    };
 
     # delta pulls in bat
     # bat complains about not seeing catppuccin mocha
