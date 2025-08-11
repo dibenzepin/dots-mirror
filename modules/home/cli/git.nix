9f1951191ccdf7718135c300e322d670eb836ca7
fumnanya <fmowete@outlook.com>
2025-08-11 22:55:07 +0100

    make jj waste time for the plot

--- modules/home/cli/git.nix
+++ modules/home/cli/git.nix
@@ -59,7 +59,7 @@
             detect_folders = [ ".jj" ];
             symbol = "🥋 ";
             command = ''
-              jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+              jj log --revisions @ --no-graph --color always --limit 1 --template '
                 separate(" ",
                   change_id.shortest(4),
                   bookmarks,
